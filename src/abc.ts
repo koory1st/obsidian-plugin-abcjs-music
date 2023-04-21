16034f02c366de1f2b79cf0f7704696d823e94f5
koory1st <32436334@qq.com>
2023-04-21 21:35:04 +0800

    fix: update abcjs version

--- src/abc.ts
+++ src/abc.ts
@@ -2,10 +2,7 @@ import * as ABCJS from 'abcjs'
 import { parse } from 'path';
 import { parentPort } from 'worker_threads';
 
-const abcOptions = {
-  add_classes: true,
-  responsive: "resize"
-};
+
 
 class CursorControlClass implements ABCJS.CursorControl {
   paperEl: HTMLElement
@@ -24,10 +21,10 @@ class CursorControlClass implements ABCJS.CursorControl {
     }
     var cursor = document.createElementNS("http://www.w3.org/2000/svg", "line");
     cursor.setAttribute("class", "abcjs-cursor");
-    cursor.setAttributeNS(null, 'x1', 0);
-    cursor.setAttributeNS(null, 'y1', 0);
-    cursor.setAttributeNS(null, 'x2', 0);
-    cursor.setAttributeNS(null, 'y2', 0);
+    cursor.setAttributeNS(null, 'x1', '0');
+    cursor.setAttributeNS(null, 'y1', '0');
+    cursor.setAttributeNS(null, 'x2', '0');
+    cursor.setAttributeNS(null, 'y2', '0');
 
     svg.appendChild(cursor);
   }
@@ -39,10 +36,10 @@ class CursorControlClass implements ABCJS.CursorControl {
     }
     var cursor = this.paperEl.querySelector("svg .abcjs-cursor");
     if (cursor) {
-      cursor.setAttribute("x1", 0);
-      cursor.setAttribute("x2", 0);
-      cursor.setAttribute("y1", 0);
-      cursor.setAttribute("y2", 0);
+      cursor.setAttribute("x1", '0');
+      cursor.setAttribute("x2", '0');
+      cursor.setAttribute("y1", '0');
+      cursor.setAttribute("y2", '0');
     }
   }
   onEvent(event: ABCJS.NoteTimingEvent): void {
@@ -53,6 +50,9 @@ class CursorControlClass implements ABCJS.CursorControl {
     for (var k = 0; k < lastSelection.length; k++)
       lastSelection[k].classList.remove("highlight");
 
+    if (!event || !event.elements) {
+      return;
+    }
     for (var i = 0; i < event.elements.length; i++) {
       var note = event.elements[i];
       for (var j = 0; j < note.length; j++) {
@@ -60,36 +60,50 @@ class CursorControlClass implements ABCJS.CursorControl {
       }
     }
 
+    if (!event.left || !event.top || !event.height) {
+      return;
+    }
+
     var cursor = this.paperEl.querySelector("svg .abcjs-cursor");
     if (cursor) {
-      cursor.setAttribute("x1", event.left - 2);
-      cursor.setAttribute("x2", event.left - 2);
-      cursor.setAttribute("y1", event.top);
-      cursor.setAttribute("y2", event.top + event.height);
+      let x = String(event.left - 2);
+      cursor.setAttribute("x1", x);
+      cursor.setAttribute("x2", x);
+      let y1 = String(event.top);
+      cursor.setAttribute("y1", y1);
+      let y2 = String(event.top + event.height);
+      cursor.setAttribute("y2", y2);
     }
   }
 }
 
 
-export function load(paperEl: HTMLElement, audioEl: HTMLElement, source: String) {
+export function load(paperEl: HTMLElement, audioEl: HTMLElement, source: string) {
   console.log("abc load method");
   console.log(paperEl);
   console.log(audioEl);
+
+  if (!ABCJS.synth.supportsAudio()) {
+    audioEl.innerHTML = "<div class='audio-error'>Audio is not supported in this browser.</div>";
+    return;
+  }
 
 
 
   let cursorControl = new CursorControlClass(paperEl, audioEl);
 
-  let synthControl;
+  let synthControl: ABCJS.SynthObjectController;
 
-  if (ABCJS.synth.supportsAudio()) {
-    synthControl = new ABCJS.synth.SynthController();
-    synthControl.load(audioEl, cursorControl, { displayLoop: true, displayRestart: true, displayPlay: true, displayProgress: true, displayWarp: true });
-  } else {
-    audioEl.innerHTML = "<div class='audio-error'>Audio is not supported in this browser.</div>";
-  }
+  synthControl = new ABCJS.synth.SynthController();
+
+  const synthOption: ABCJS.SynthVisualOptions = { displayLoop: true, displayRestart: true, displayPlay: true, displayProgress: true, displayWarp: true }
+  synthControl.load(audioEl, cursorControl, synthOption);
   synthControl.disable(true);
 
+  const abcOptions: ABCJS.AbcVisualParams = {
+    add_classes: true,
+    responsive: "resize"
+  };
   var visualObj = ABCJS.renderAbc(paperEl, source, abcOptions)[0];
 
   var midiBuffer = new ABCJS.synth.CreateSynth();
@@ -98,13 +112,11 @@ export function load(paperEl: HTMLElement, audioEl: HTMLElement, source: String)
     visualObj: visualObj,
   }).then(function(response) {
     console.log(response);
-    if (synthControl) {
-      synthControl.setTune(visualObj, false).then(function(response) {
-        console.log("Audio successfully loaded.")
-      }).catch(function(error) {
-        console.warn("Audio problem:", error);
-      });
-    }
+    synthControl.setTune(visualObj, false).then(function(_) {
+      console.log("Audio successfully loaded.")
+    }).catch(function(error) {
+      console.warn("Audio problem:", error);
+    });
   }).catch(function(error) {
     console.warn("Audio problem:", error);
   });
